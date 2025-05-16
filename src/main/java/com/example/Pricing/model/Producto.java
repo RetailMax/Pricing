7ccee7a7f826ba6c98742b875ffc444dcbbd0d07
Abrah <ab.herreraa@duocuc.cl>
2025-05-16 00:07:29 -0400

    Se realizó la conección a una base de datos en la nube para almacenar los precios y descuentos obtenidos

--- src/main/java/com/example/Pricing/model/Producto.java
+++ src/main/java/com/example/Pricing/model/Producto.java
@@ -3,16 +3,23 @@ package com.example.Pricing.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
 
-
+@Entity
+@Table(name = "producto")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Producto {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = true)
     private String descripcion;
 
 }
