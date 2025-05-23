bad3c27bcb04db72848ab6df4e2d7eeb7ba2174d
Abrah <ab.herreraa@duocuc.cl>
2025-05-22 20:08:06 -0400

    Se cambió la extension de la api

--- src/main/java/com/example/Pricing/controller/PricingController.java
+++ src/main/java/com/example/Pricing/controller/PricingController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/precios")
+@RequestMapping("/api/precios/base")
 public class PricingController {
     
     private final PrecioBaseRepository precioBaseRepository;
